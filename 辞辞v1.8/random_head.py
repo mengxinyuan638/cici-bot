6f97de978b0297b77944d1c435b5c64507a616f6
GC-ZF <zhang15835765326@gmail.com>
2022-07-19 20:03:50 +0800

    重构随机头像

--- 辞辞v1.8/random_head.py
+++ 辞辞v1.8/random_head.py
@@ -1,23 +1,28 @@
 from nonebot import on_regex
 from nonebot.typing import T_State
-from nonebot.adapters.onebot.v11 import Bot,Message,Event
+from nonebot.adapters.onebot.v11 import Bot, Message, Event
 import requests
 import json
 
-random_head = on_regex(pattern = r'^随机头像$')
+'''
+随机头像 调用API https://www.yuanxiapi.cn/api/touxiang/?format=json
+正则命令:随机头像
+'''
+random_head = on_regex ( pattern=r'^随机头像$' )
 
-@random_head.handle()
+
+@random_head.handle ()
 async def sj(bot: Bot, event: Event, state: T_State):
-    get_img = await head()
+    get_img = await head ()
     # at_ = f"[CQ:at,qq={event.get_user_id()}]"
-    await random_head.send(Message(get_img))
+    await random_head.send ( Message ( get_img ) )
 
 
 async def head():
     url = 'https://www.yuanxiapi.cn/api/touxiang/?format=json'
-    get_data = requests.get(url)
-    get_json = json.loads(get_data.text)
+    get_data = requests.get ( url )
+    get_json = json.loads ( get_data.text )
     # print(get_json)
-    img = get_json['imgurl']
+    img = get_json[ 'imgurl' ]
     wb = f"[CQ:image,file={img}]"
     return wb

724fbd66cd5aa6c9c4bb1c848f2c80bf3f1249df
GC_Zhang <1310446718@qq.com>
2022-08-14 12:39:13 +0000

    update 辞辞v2.0修复随机歌曲/qunguan.py. 修改原先通过变量zr = 1648576390去控制操作权限，别人下上没法用 改为permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER，即超管、群管、群主具有操作权限
    Signed-off-by: GC_Zhang <1310446718@qq.com>

--- 辞辞v2.0修复随机歌曲/qunguan.py
+++ 辞辞v2.0修复随机歌曲/qunguan.py
@@ -1,139 +1,128 @@
-from email import message
-from nonebot import on_keyword, on_regex, on_request,on_command
+import random
+from nonebot import on_keyword, on_request, on_command
+from nonebot.exception import ActionFailed
 from nonebot.typing import T_State
-from nonebot.adapters.onebot.v11 import GroupMessageEvent,Bot,GroupRequestEvent
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot, GroupRequestEvent, GROUP_ADMIN, GROUP_OWNER
 import warnings
 from nonebot.permission import *
 import re
 
-
-warnings.filterwarnings("ignore")
+warnings.filterwarnings ( "ignore" )
 
 # 撤回消息
-che = on_keyword({'广告', '沙雕', '广告', 'md', '妈的', '卧槽', '嘛的', '操你妈', '操你', '加vx','操','草','傻逼','SB','sb','nt','脑瘫','骚','CNM','CNm','CnM','Cnm','cNM','cNm','cnM'})
-qtk = on_regex(pattern = r'^开启全禁$')
-qtg = on_regex(pattern = r'^关闭全禁$')
-jy = on_keyword({'禁'})
-jj = on_keyword({'解'})
-group_remove = on_keyword({'踢'})
-group_sq = on_request()
-agree_apply = on_regex(pattern = r'^同意申请$')
-disagree_apply = on_regex(pattern = r'^拒绝申请$')
-zr = 1648576390
+che = on_keyword (
+    {'广告', '沙雕', '广告', 'md', '妈的', '卧槽', '嘛的', '操你妈', '操你', '加vx', '操', '草', '傻逼', 'SB', 'sb', 'nt', '脑瘫', '骚', 'CNM',
+     'CNm', 'CnM', 'Cnm', 'cNM', 'cNm', 'cnM'} )
+all_ban = on_command ( "开启全禁", priority=1, block=True, permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER )
+all_unban = on_command ( "关闭全禁", priority=1, block=True, permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER )
+alone_ban = on_command ( "禁", priority=1, block=True, permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER )
+alone_unban = on_command ( "解", priority=1, block=True, permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER )
+group_remove = on_command ( "踢", priority=1, block=True, permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER )
+group_apply = on_request ()
+agree_apply = on_command ( "同意", priority=1, block=True, permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER )
+disagree_apply = on_command ( "拒绝", priority=1, block=True, permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER )
 
 
-
-@che.handle()
+@che.handle ()
 async def c(bot: Bot, event: GroupMessageEvent, state: T_State):
     mid = event.message_id
     group = event.group_id
     qq = event.user_id
-    sj = 300
-    print(mid)
-    print(group)
-    await bot.delete_msg(message_id=mid)
-    await bot.set_group_ban(group_id=group,user_id=qq,duration=sj)
-    await che.finish(message=f'@{qq} 你的发言可能包含敏感词汇，这里禁言5分钟警告一下')
+    ban = 300  # 禁言300s
+    print ( mid )
+    print ( group )
+    await bot.delete_msg ( message_id=mid )  # 撤回消息
+    await bot.set_group_ban ( group_id=group, user_id=qq, duration=ban )  # 禁言
+    await che.finish ( message=f'@{qq} 你的发言可能包含敏感词汇，这里禁言5分钟警告一下' )
 
-@qtk.handle()
-async def j(bot: Bot, event: GroupMessageEvent, state: T_State):
-    group = event.group_id
-    qq = event.user_id
-    if qq == zr:
-        await bot.set_group_whole_ban(group_id=group,enable=True)
-        await che.finish(message=f'好的大大，辞辞已经为您开启全体禁言了')
-    else:
-        await che.finish(message=f'你没有资格命令我！')
 
-@qtg.handle()
-async def g(bot: Bot, event: GroupMessageEvent, state: T_State):
-    group = event.group_id#获取当前群号
-    qq = event.user_id
+# 开启全员禁言
+@all_ban.handle ()
+async def ban_all(bot: Bot, event: GroupMessageEvent, state: T_State):
     try:
-        if qq == zr:
-            await bot.set_group_whole_ban(group_id=group,enable=False)
-            await che.finish(message=f'好的大大，辞辞已经为您关闭全体禁言了')
-        else:
-            await che.finish(message=f'你没有资格命令我！')
-    except IndexError:
-        k = 'error'
+        await bot.set_group_whole_ban ( group_id=event.group_id, enable=True )
+        await all_ban.finish ( "已开启全员禁言" )
+    except ActionFailed:
+        await all_ban.finish ( "权限不足" )
 
 
+# 关闭全员禁言
+@all_unban.handle ()
+async def unban_all(bot: Bot, event: GroupMessageEvent, state: T_State):
+    try:
+        await bot.set_group_whole_ban ( group_id=event.group_id, enable=False )
+        await all_unban.finish ( "已关闭全员禁言" )
+    except ActionFailed:
+        await all_unban.finish ( "权限不足" )
 
 
-@jy.handle()
+# 禁言某人
+@alone_ban.handle ()
+async def ban_alone(bot: Bot, event: GroupMessageEvent, state: T_State):
+    try:
+        data = str ( event.message )
+        qq = re.findall ( r'qq=(.+?)]', data )
+        qq = int ( qq[ 0 ] )
+        minute = random.randint ( 1, 10 )
+        seconds = minute*60
+        await bot.set_group_ban ( group_id=event.group_id, user_id=qq, duration=seconds )
+        await alone_ban.finish ( message=f'已禁言\nQQ:{qq}\n时间：{minute}分钟' )
+    except ActionFailed:
+        await alone_ban.finish ( "权限不足" )
+
+
+# 解禁某人
+@alone_unban.handle ()
 async def sj(bot: Bot, event: GroupMessageEvent, state: T_State):
     try:
-        data = str(event.message)
-        qq2 = event.user_id#获取用户id
-        qq = re.findall(r'qq=(.+?)]',data)
-        qq = int(qq[0])
-        sj1 = re.findall(r'] (.+)',data)
-        sj = sj1[0]
-        sj = int(sj)*60
-        if qq2 == zr:
-            await bot.set_group_ban(group_id=event.group_id,user_id=qq,duration=sj)
-            await jy.finish(message=f'好的大大，辞辞已禁言\nQQ:{qq}\n时间：{sj1[0]}分钟')
-        else:
-            await jy.finish(message=f'你没有资格命令我！')
-    except IndexError:
-        print('Error')
+        data = str ( event.message )
+        qq = re.findall ( r'qq=(.+?)]', data )
+        qq = int ( qq[ 0 ] )
+        await bot.set_group_ban ( group_id=event.group_id, user_id=qq, duration=0 )
+        await alone_unban.finish ( message=f'已解除禁言\nQQ:{qq}' )
+    except ActionFailed:
+        await alone_unban.finish ( "权限不足" )
 
-@jj.handle()
-async def sj(bot: Bot, event: GroupMessageEvent, state: T_State):
-    try:
-        data = str(event.message)
-        qq2 = event.user_id#获取用户id
-        qq = re.findall(r'qq=(.+?)]',data)
-        qq = int(qq[0])
-        if qq2 == zr:
-            await bot.set_group_ban(group_id=event.group_id,user_id=qq,duration=0)
-            await jj.finish(message=f'好的大大，辞辞已解除禁言\nQQ:{qq}')
-        else:
-            await jj.finish(message=f'你没有资格命令我！')
-    except IndexError:
-        print('Error')
-    
-    
-@group_sq.handle()
-async def sq(bot: Bot, event: GroupRequestEvent, state: T_State):
-    qq_id = event.user_id
-    message = event.comment#获取验证信息
+
+# 入群申请消息
+@group_apply.handle ()
+async def apply_msg(bot: Bot, event: GroupRequestEvent, state: T_State):
+    global apply_id
+    apply_id = event.user_id
+    message = event.comment  # 获取验证信息
     global flag_id
-    flag_id = event.flag#申请进群的flag
+    flag_id = event.flag  # 申请进群的flag
     global type_id
-    type_id = event.sub_type#请求信息的类型
-    await group_sq.finish(message=f'有人申请进群\nQQ:{qq_id}\n验证消息:{message}\n同意/拒绝申请')
+    type_id = event.sub_type  # 请求信息的类型
+    await group_apply.finish ( message=f'有人申请进群\nQQ:{apply_id}\n验证消息:{message}\n同意/拒绝' )
 
 
-@agree_apply.handle()
-async def sn(bot: Bot, event: GroupMessageEvent, state: T_State):    
-    if event.sender.role == "admin" or event.sender.role == "owner":
-        await bot.set_group_add_request(flag=flag_id,sub_type=type_id,approve=True)
-    else:
-        await group_remove.finish(message=f'对不起，你没有权限')
+# 同意申请入群
+@agree_apply.handle ()
+async def agree(bot: Bot, event: GroupMessageEvent, state: T_State):
+    try:
+        await bot.set_group_add_request ( flag=flag_id, sub_type=type_id, approve=True )
+        await agree_apply.finish ( message=f'欢迎{apply_id}入群' )
+    except ActionFailed:
+        await agree_apply.finish ( "权限不足" )
 
-@disagree_apply.handle()
-async def sn(bot: Bot, event: GroupMessageEvent, state: T_State):    
-    if event.sender.role == "admin" or event.sender.role == "owner":
-        await bot.set_group_add_request(flag=flag_id,sub_type=type_id,approve=False,reason='机器人自动审批，如有误判请联系群主或其他管理员')
-    else:
-        await group_remove.finish(message=f'对不起，你没有权限')
-    
-@group_remove.handle()
+
+# 拒绝入群
+@disagree_apply.handle ()
+async def sn(bot: Bot, event: GroupMessageEvent, state: T_State):
+    await bot.set_group_add_request ( flag=flag_id, sub_type=type_id, approve=False,
+                                      reason='机器人自动审批，如有误判请联系群主或其他管理员' )
+
+
+# 踢出群聊
+@group_remove.handle ()
 async def move(bot: Bot, event: GroupMessageEvent, state: T_State):
-    async def group_admin(event: GroupMessageEvent) -> bool:
-        return event.sender.role == "admin"
-    async def group_owner(event: GroupMessageEvent) -> bool:
-        return event.sender.role == "owner"
-    print(group_admin(event))
-    if event.sender.role == "admin" or event.sender.role == "owner":
-        data = str(event.message)
+    try:
+        data = str ( event.message )
         group_id = event.group_id
-        qq_id = re.findall(r'qq=(.+?)]',data)
-        qq_id = qq_id[0]
-        await bot.set_group_kick(group_id=group_id,user_id=qq_id,reject_add_request=False)
-        await group_remove.finish(message=f'已将QQ:{qq_id}移除群聊')
-    else:
-        await group_remove.finish(message=f'对不起，你没有权限')
-    
+        qq_id = re.findall ( r'qq=(.+?)]', data )
+        qq_id = qq_id[ 0 ]
+        await bot.set_group_kick ( group_id=group_id, user_id=qq_id, reject_add_request=False )
+        await group_remove.finish ( message=f'已将QQ:{qq_id}移除群聊' )
+    except ActionFailed:
+        await group_remove.finish ( "权限不足" )
